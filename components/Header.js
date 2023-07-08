06bf29c45a58b5d097f135ede96d4136f93e3421
Adarsha-React <adarshareact2023@gmail.com>
2023-07-09 00:01:06 +0530

    branch-001 - Header changes

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,12 @@ const Header = () => {
   return (
     <div className="header">
       <h1>Header component</h1>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </div>
     </div>
   );
 };
